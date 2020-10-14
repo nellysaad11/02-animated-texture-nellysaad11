78d472b9b2b37c98d9e54a2417e30972559fa287
nellysaad11 <64480661+nellysaad11@users.noreply.github.com>
2020-10-14 06:32:06 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,24 @@
 
-function preload(){
-  // put preload code here
-}
-
 var colorList1 =['#DC7341','#D75F26','#DA4B07','#BD6034','#C14910'];
 var colorList2= ['#FF5300','#FF732F','#FC9562','#FEAB83','#FCCAB2'];
+var colorList3= ['#752601','#87492B','#9C6A51', '#99441A', '#BD4104'];
 
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
   // put setup code here
-}
-
+  bgColor = random(colorList1);
+  rectColor = random(colorList1);
 }
 function draw() {
   // put drawing code here
-  background('#FFEDE5');
+  background(bgColor);
+
 
   for (var a = 0; a < windowWidth; a += 100) {
   for (var b = 0; b < windowHeight;b += 100) {
   for (var c = 0;c < windowWidth; c += 200) {
   for (var d = 0;d < windowHeight; d += 200) {
-
-
 
       fill(random(colorList1));
       rect(a, b, 70,100);
@@ -30,15 +26,19 @@ function draw() {
       fill(random(colorList2));
       rect(a, b, 100,50);
       noStroke();
-      fill(random(colorList2));
+      fill(rectColor);
       rect(b, c, 1000,10);
       noStroke();
-      fill(random(colorList1));
+      fill(rectColor);
       rect(a, d, 10,1000);
       noStroke();
       rectMode(CENTER);
+      if (mouseIsPressed) {
+      bgColor= color(random(colorList1));
+      rectColor= color(random(colorList3));
 
-    }
+}
+}
 }
 }
 }
